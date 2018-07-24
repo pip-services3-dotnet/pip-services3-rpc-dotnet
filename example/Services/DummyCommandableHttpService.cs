d0d566b9076e4f58ea5896d831c7f2f65a3d94b6
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-07-23 20:16:56 -0700

    Initial commit

---
+++ example/Services/DummyCommandableHttpService.cs
@@ -0,0 +1,13 @@
+﻿using PipServices.Commons.Refer;
+
+namespace PipServices.Rpc.Rest
+{
+    public sealed class DummyCommandableHttpService : CommandableHttpService
+    {
+        public DummyCommandableHttpService() 
+            : base("dummy")
+        {
+            _dependencyResolver.Put("controller", new Descriptor("pip-services-dummies", "controller", "default", "*", "1.0"));
+        }
+    }
+}
---
+++ example/Services/DummyCommandableHttpServiceV2.cs
@@ -0,0 +1,22 @@
+﻿using PipServices.Commons.Refer;
+
+namespace PipServices.Rpc.Rest
+{
+    public sealed class DummyCommandableHttpServiceV2 : CommandableHttpService
+    {
+        private IDummyController _controller;
+
+        public DummyCommandableHttpServiceV2() 
+            : base("Dummy")
+        {
+            _dependencyResolver.Put("controller", new Descriptor("pip-services-dummies", "controller", "default", "*", "1.0"));
+        }
+
+        public override void SetReferences(IReferences references)
+        {
+            base.SetReferences(references);
+
+            _controller = references.GetOneRequired<IDummyController>(new Descriptor("pip-services-dummies", "controller", "default", "*", "1.0"));
+        }
+    }
+}
