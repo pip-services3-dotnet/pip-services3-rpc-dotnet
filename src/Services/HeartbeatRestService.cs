91948866c5305097478a479812f050470f3be53a
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-09-24 18:18:01 -0700

    Improved style in examples

---
+++ src/Services/HeartbeatRestService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using PipServices.Commons.Config;
+
+namespace PipServices.Rpc.Services
+{
+    public class HeartbeatRestService: RestService
+    {
+        private string _route = "heartbeat";
+
+        public HeartbeatRestService()
+        {
+        }
+
+        public override void Configure(ConfigParams config)
+        {
+            base.Configure(config);
+
+            _route = config.GetAsStringWithDefault("route", _route);
+        }
+
+        public override void Register()
+        {
+            RegisterRoute("get", _route, Heartbeat);
+        }
+
+        private async Task Heartbeat(HttpRequest request, HttpResponse response, RouteData routeData)
+        {
+            await SendResultAsync(response, DateTime.UtcNow);
+        }
+    }
+}
---
+++ src/Services/StatusRestService.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using PipServices.Commons.Config;
+using PipServices.Commons.Convert;
+using PipServices.Commons.Refer;
+using PipServices.Components.Info;
+
+namespace PipServices.Rpc.Services
+{
+    public class StatusRestService: RestService
+    {
+        private DateTime _startTime = DateTime.UtcNow;
+        private IReferences _references;
+        private ContextInfo _contextInfo;
+        private string _route = "status";
+
+        public StatusRestService()
+        {
+            _dependencyResolver.Put("context-info", new Descriptor("pip-services", "context-info", "default", "*", "1.0"));
+        }
+
+        public override void Configure(ConfigParams config)
+        {
+            base.Configure(config);
+
+            _route = config.GetAsStringWithDefault("route", _route);
+        }
+
+		public override void SetReferences(IReferences references)
+		{
+            _references = references;
+            base.SetReferences(references);
+
+            _contextInfo = _dependencyResolver.GetOneOptional<ContextInfo>("context-info");
+		}
+
+		public override void Register()
+        {
+            RegisterRoute("get", _route, Status);
+        }
+
+        private async Task Status(HttpRequest request, HttpResponse response, RouteData routeData)
+        {
+            var id = _contextInfo != null ? _contextInfo.ContextId : "";
+            var name = _contextInfo != null ? _contextInfo.Name : "Unknown";
+            var description = _contextInfo != null ? _contextInfo.Description : "";
+            var uptime = (DateTime.UtcNow - _startTime).TotalMilliseconds;
+            var properties = _contextInfo.Properties;
+
+            var components = new List<string>();
+            if (_references != null)
+            {
+                foreach (var locator in _references.GetAllLocators())
+                    components.Add(locator.ToString());
+            }
+
+            var status = new
+            {
+                id = id,
+                name = name,
+                description = description,
+                start_time = StringConverter.ToString(_startTime),
+                current_time = StringConverter.ToString(DateTime.UtcNow),
+                uptime = uptime,
+                properties = properties,
+                components = components
+            };
+
+            await SendResultAsync(response, status);
+        }
+    
+    }
+}
