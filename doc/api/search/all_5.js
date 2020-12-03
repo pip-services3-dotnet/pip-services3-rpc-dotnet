ef4b1ac9326eab3ea26da6c47e78fff529c2e435
banalna <judas.priest999@gmail.com>
2020-12-03 10:45:36 +0300

    delete component.conf.ps1 and doc folder

--- doc/api/search/all_5.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['heartbeat',['Heartbeat',['../class_pip_services_1_1_rpc_1_1_services_1_1_heartbeat_rest_service.html#a263dbeff7770cf03c9faa2d045a33b8a',1,'PipServices3::Rpc::Services::HeartbeatRestService']]],
-  ['heartbeatrestservice',['HeartbeatRestService',['../class_pip_services_1_1_rpc_1_1_services_1_1_heartbeat_rest_service.html',1,'PipServices3.Rpc.Services.HeartbeatRestService'],['../class_pip_services_1_1_rpc_1_1_services_1_1_heartbeat_rest_service.html#adc3dbc6e4ab522d7c53ebb67b13f4f2a',1,'PipServices3.Rpc.Services.HeartbeatRestService.HeartbeatRestService()']]],
-  ['httpconnectionresolver',['HttpConnectionResolver',['../class_pip_services_1_1_rpc_1_1_connect_1_1_http_connection_resolver.html',1,'PipServices3::Rpc::Connect']]],
-  ['httpendpoint',['HttpEndpoint',['../class_pip_services_1_1_rpc_1_1_services_1_1_http_endpoint.html',1,'PipServices3::Rpc::Services']]],
-  ['httpresponsesender',['HttpResponseSender',['../class_pip_services_1_1_rpc_1_1_services_1_1_http_response_sender.html',1,'PipServices3::Rpc::Services']]]
-];
--- doc/api/search/classes_1.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['defaultrpcfactory',['DefaultRpcFactory',['../class_pip_services_1_1_rpc_1_1_build_1_1_default_rpc_factory.html',1,'PipServices3::Rpc::Build']]],
-  ['directclient',['DirectClient',['../class_pip_services_1_1_rpc_1_1_clients_1_1_direct_client.html',1,'PipServices3::Rpc::Clients']]]
-];
