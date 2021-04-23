40a081d90f882f48d38350f117f32b1d24076c25
Sergey Seroukhov <seroukhov@gmail.com>
2021-04-22 21:25:28 -0700

    Added TestRpcClient and TestCommandableHttpClient

---
+++ src/Test/TestCommandableHttpClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PipServices3.Rpc.Test
+{
+    public class TestCommandableHttpClient
+    {
+        public TestCommandableHttpClient()
+        {
+        }
+    }
+}
---
+++ src/Test/TestRestClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PipServices3.Rpc.Test
+{
+    public class TestRestClient
+    {
+        public TestRestClient()
+        {
+        }
+    }
+}
